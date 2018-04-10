37b3539460df6c12133b332522a91b06b5b180f2
Michelle Olivieri <michelleo72@gmail.com>
2018-04-10 14:00:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.detect { |food| cheese_types.include? food }
+  foods.detect { |food| cheese_types.include? food } ? true : false
 end
