dc376ff4926a20fcb387daf8d0add21248ce4574
Michelle Olivieri <michelleo72@gmail.com>
2018-04-10 13:45:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.detect { |call| call.length > 4 } ? true : false
+  #calls.detect { |call| call.length > 4 } ? true : false
+  calls.detect { |call| call.length > 4 } 
 end
 
 def find_the_cheese(foods)
